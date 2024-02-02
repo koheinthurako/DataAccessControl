b7d1a0126fc79cf53a8bfe1c06c14d3d797fe62a
koheinthurako <captainthura99@gmail.com>
2024-02-02 22:41:38 +0630

    delete test files

--- src/main/java/com/dbtest/DBDemo/Repo/TestRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dbtest.DBDemo.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dbtest.DBDemo.entities.Test;
-
-public interface TestRepo extends JpaRepository<Test, Integer>{
-	
-}
